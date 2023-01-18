8dd5c94471d05251e1cd4bf3db041c399c5bafe2
bky4564 <bky4564@hunet.co.kr>
2023-01-18 15:01:03 +0900

    Initial commit

---
+++ src/test/java/com/yunbaek/springreactive/SpringReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunbaek.springreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
